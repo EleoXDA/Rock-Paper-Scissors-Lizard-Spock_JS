6bac1dc02bc27fd809fc1bd6326462e351538bcb
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2022-10-09 16:06:58 +0200

    configure user choice options

--- app.js
+++ app.js
@@ -5,6 +5,20 @@ const PAPER = 'PAPER'
 const SCISSORS = 'SCISSORS'
 const DEFAULT_CHOICE = ROCK
 
+const getUserChoice = function() {
+  const selection = prompt(`${ROCK}, ${PAPER} or ${SCISSORS}?`, '').toUpperCase();
+  // uppercase will make sure to accept user options when it is not in capitals
+  if (
+    selection !== ROCK &&
+    selection !== PAPER &&
+    selection !== SCISSORS
+  ) {
+    alert(`Invalid choice! We chose ${DEFAULT_CHOICE} for you!`);
+    return DEFAULT_CHOICE
+  }
+  return selection;
+};
+
 startGameBtn.addEventListener('click', function startText() {
   console.log('Game is starting...');
 });
