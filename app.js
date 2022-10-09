f4899b7ed99bd4cb5c707289c9fe52e4b871eb4e
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2022-10-09 16:05:15 +0200

    user options & a default choice for invalid choice

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const startGameBtn = document.getElementById('start-game-btn');
 
+const ROCK = 'ROCK'
+const PAPER = 'PAPER'
+const SCISSORS = 'SCISSORS'
+const DEFAULT_CHOICE = ROCK
+
 startGameBtn.addEventListener('click', function startText() {
   console.log('Game is starting...');
 });
