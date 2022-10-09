6b98e4682df8f43ed59c687b0c86163a766c50d3
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2022-10-09 19:36:22 +0200

    fix game logic and mispronunciations

--- app.js
+++ app.js
@@ -33,27 +33,29 @@ const getBotChoice = () => {
   }
 };
 
-const getWinner = (bChoice, uChoice = DEFAULT_CHOICE) => // when userChoice is undefined, DEFAULT_CHOICE will override it
+const getWinner = (
+  bChoice,
+  uChoice = DEFAULT_CHOICE // when userChoice is undefined, DEFAULT_CHOICE will override it
+) =>
   bChoice === uChoice
-    ? RESULT_DRAW 
-    : (bChoice === ROCK && uChoice === PAPER) || 
-      (bChoice === PAPER && uChoice === SCISSORS) || 
-      (bChoice === SCISSORS && uChoice === ROCK) 
-    ? RESULT_USER_WINS 
+    ? RESULT_DRAW
+    : (bChoice === ROCK && uChoice === PAPER) ||
+      (bChoice === PAPER && uChoice === SCISSORS) ||
+      (bChoice === SCISSORS && uChoice === ROCK)
+    ? RESULT_USER_WINS
     : RESULT_BOT_WINS;
 
-  // if (bChoice === uChoice) {
-  //   return RESULT_DRAW;
-  // } else if (
-  //   (bChoice === ROCK && uChoice === PAPER) ||
-  //   (bChoice === PAPER && uChoice === SCISSORS) ||
-  //   (bChoice === SCISSORS && uChoice === ROCK)
-  // ) {
-  //   return RESULT_USER_WINS;
-  // } else {
-  //   return RESULT_BOT_WINS;
-  // }
-
+// if (bChoice === uChoice) {
+//   return RESULT_DRAW;
+// } else if (
+//   (bChoice === ROCK && uChoice === PAPER) ||
+//   (bChoice === PAPER && uChoice === SCISSORS) ||
+//   (bChoice === SCISSORS && uChoice === ROCK)
+// ) {
+//   return RESULT_USER_WINS;
+// } else {
+//   return RESULT_BOT_WINS;
+// }
 
 startGameBtn.addEventListener('click', () => {
   if (gameIsRunning) {
@@ -63,13 +65,16 @@ startGameBtn.addEventListener('click', () => {
   console.log('Game is starting...');
   const userSelection = getUserChoice(); // might be undefined when default option is removed
   const botSelection = getBotChoice();
+  let winner;
   if (userSelection) {
-    const winner = getWinner(userSelection, botSelection);
+    winner = getWinner(botSelection, userSelection);
   } else {
     winner = getWinner(botSelection, userSelection); // we only pass one variable in function, but it still gets forgiven by JS
   }
-  let message = `You picked ${userSelection || DEFAULT_CHOICE}, bot picked ${botSelection}, so you`;
-  if (winner === RESTULT_DRAW) {
+  let message = `You picked ${
+    userSelection || DEFAULT_CHOICE
+  }, bot picked ${botSelection}, so you `;
+  if (winner === RESULT_DRAW) {
     message = message + 'had a draw.';
   } else if (winner === RESULT_USER_WINS) {
     message = message + 'won.';
