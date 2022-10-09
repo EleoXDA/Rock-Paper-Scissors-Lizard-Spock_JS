03a9c4b8f4ecdd4cb415c141609dea26550a8164
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2022-10-09 12:03:02 +0200

    create function stored in object + can be executed

--- app.js
+++ app.js
@@ -4,4 +4,12 @@ function startGame() {
   console.log('Game is starting...');
 }
 
+const person = {
+  greet: function greet() {
+    console.log('Hello there!');
+  }
+}
+
+person.greet();
+
 startGameBtn.addEventListener('click', startGame)
