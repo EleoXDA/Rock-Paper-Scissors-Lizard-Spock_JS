82f5e4a0bb9c41864e068a9970cb0d934bc37771
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2022-10-09 15:46:12 +0200

    another way of usage for anonymous functions

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const startGameBtn = document.getElementById('start-game-btn');
 // if you store a function in right side of equation -> it is no longer global and you need to call the left-side variable/constant name
 // it is now an expression, instead of declaration/statement
 // if function name is removed -> it is called an anonymous function
-const start = function() {
-  console.log('Game is starting...');
-};
+// const start = function() {
+//   console.log('Game is starting...');
+// };
 
 // const person = {
 //   greet: function greet() {
@@ -16,7 +16,9 @@ const start = function() {
 // person.greet();
 
 //event listener is also a method on startGameBtn object
-startGameBtn.addEventListener('click', start)
+startGameBtn.addEventListener('click', function() {
+  console.log('Game is starting...');
+});
 
 // console.log(typeof startGame); //will reveal that it is a function, not an object
 // console.dir(typeof startGame); //will reveal that function is an object that has key-value pair properties
