988ace2424612a00c748c0c7aa520812228821ca
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2022-10-09 16:22:53 +0200

    clean code + prettier formatting

--- app.js
+++ app.js
@@ -10,23 +10,21 @@ const RESULT_BOT_WINS = 'BOT_WINS';
 
 let gameIsRunning = false;
 
-const getUserChoice = function() {
-  const selection = prompt(`${ROCK}, ${PAPER} or ${SCISSORS}?`, '').toUpperCase();
-  // uppercase will make sure to accept user options when it is not in capitals
-  if (
-    selection !== ROCK &&
-    selection !== PAPER &&
-    selection !== SCISSORS
-  ) {
+const getUserChoice = function () {
+  const selection = prompt(
+    `${ROCK}, ${PAPER} or ${SCISSORS}?`,
+    ''
+  ).toUpperCase(); // uppercase will make sure to accept user options when it is not in capitals
+  if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
     alert(`Invalid choice! We chose ${DEFAULT_CHOICE} for you!`);
-    return DEFAULT_CHOICE
+    return DEFAULT_CHOICE;
   }
   return selection;
 };
 
-const getBotChoice = function() {
+const getBotChoice = function () {
   const randomValue = Math.random();
-  if (randomValue < 0.34){
+  if (randomValue < 0.34) {
     return ROCK;
   } else if (randomValue < 0.67) {
     return PAPER;
@@ -50,7 +48,7 @@ const getWinner = function (bChoice, uChoice) {
 };
 
 startGameBtn.addEventListener('click', function startText() {
-  if (gameIsRunning){
+  if (gameIsRunning) {
     return;
   }
   gameIsRunning = true;
