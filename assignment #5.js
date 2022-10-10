e819e4daa42ef94340e82a35cebf5d1e26202035
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2022-10-10 22:12:17 +0200

    start assignment 5

---
+++ assignment #5.js
@@ -0,0 +1,16 @@
+// function sayHello(name) {
+//   console.log('Hi ' + name);
+// }
+
+// sayHello();
+
+// 5-1: Rewrite the function above as an arrow function
+
+
+// 5-2: Adjust the arrow function with three different syntaxes: two arguments, no arguments, and with one returned value
+
+
+// 5-3: Add a default argument to the function you created: A fallback value for the phrase if no value is provided
+
+
+// 5-4: Add a new checkInput fn with takes unlimited strings and executes a cb fn with no argument
